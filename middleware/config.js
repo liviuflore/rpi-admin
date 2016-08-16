25973795884204fb7c95d281e5f044c839522668
Flore Liviu <liviu.flore@outlook.com>
2016-08-16 23:33:40 +0300

    load config from file

--- middleware/config.js
+++ middleware/config.js
@@ -1,22 +1,32 @@
 'use strict';
 
 var log = require('./log');
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 var config = {
-    system: {
-        webPort: 5000,
-        statsUpdateInterval: 5000,
-    },
-    transmission: {
-        host: '192.168.1.21',
-        port: 9091,
-        username: 'osmc',
-        password: 'liviu22',
-        url: '/transmission/rpc',
-    }
+  system: {
+    webPort: 5000,
+    statsUpdateInterval: 5000,
+  },
+  transmission: {
+    host: '192.168.1.21',
+    port: 9091,
+    username: 'osmc',
+    password: 'liviu22',
+    url: '/transmission/rpc',
+  }
 }
+try {
+  config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+}
+catch (e) {
+  log.e('config file not found, loading defaults');
+}
+//log.d(config);
+
+
 
 router.get('/system', function (req, res) {
     res.json(config.system);
